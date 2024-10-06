1eb21a031f12eac7cc8b8d3477125233dbc5d9b4
VellStraggler <david@incwells.com>
2024-10-06 00:20:56 -0600

    readme finished

--- README.md
+++ README.md
@@ -1,18 +1,52 @@
-# TDL Website
+## Overview
 
-## completed features:
-- fluid logo
-- stylized header links
-- minimizing header
-- photos for each section
-- working social media icon links
-- connected github repository
-- connect header links to each respective section
-- Set section headers to roll out when reached
-- published
+**Project Title**: TDL Website
 
-## features to add:
-- add services section
+**Project Description**: A website proposal made for a construction company that recently underwent a change in management.
+
+**Project Goals**: Create a website that works fluidly, looks professional, and stands out.
+
+## Instructions for Build and Use
+
+Steps to build and/or run the software:
+
+1. First get into the right directory. You should be in "/TDL-WEBSITE"
+2. Open a terminal and type npm start
+3. It should then host a local server of this website
+
+Instructions for using the software:
+
+1. Scroll to explore the site, or use the internal links at the top
+2. Alternatively, visit the public website being hosted on Github here: http://VellStraggler.github.io/TDL-site
+
+## Development Environment 
+
+To recreate the development environment, you need the following software and/or libraries with the specified versions:
+
+* Install Node.js if you don't already have it. It is found on their website: https://nodejs.org/en/
+* Open a command prompt to confirm its installation by typing this version checking command: `node -v`
+* Create a new folder for your project
+* Get to this folder through the command prompt, and type
+`npx create-react-app [project-name-of-choice]`
+* Open this project in Visual Studio or your IDE of choice
+
+## Useful Websites to Learn More
+
+I found these websites useful in developing this software:
+
+* https://chatgpt.com/
+* https://create-react-app.dev/docs/deployment/
+* https://www.w3schools.com/REACT/DEFAULT.ASP
+* https://stackoverflow.com/questions/6361074/use-html-div-to-add-border-only-on-top-and-bottom
+
+## Future Work
+
+The following items I plan to fix, improve, and/or add to this project in the future:
+
+-  Add email pop-up form
+-  adjust font-size calculations to work with small screen sizes
+-  Act on all feedback from my insider (who will help me
+  present this website to TDL Industrial)
 - multiple latest projects added (found on Linkedin)
 - Functional Contact section
 - "Vision" scrolling: the images scroll at a different pace then the rest of the site
@@ -20,7 +54,4 @@
 - Footer section expands at the bottom into the "Contact Us" section
 - Add small caption to each photo to credit where it came from
 - Add an email form
-
-## features that need help:
-- color palette
-- font resizing
+- improve the color palette
--- src/App.js
+++ src/App.js
@@ -31,44 +31,44 @@ function App() {
   }, []);
 
   return (
-    <div class="App" id = "home-section">
-      <header class="App-header" ref={headerRef} style={{height: "300px"}}>
-        <div class="inner-header">
-          <div class="logo-side" ref={logoContainer}>
-            <img src="tdllogos\TDL-logo-color-cropped.png" className="App-logo" alt="logo" />
-            <img src="tdllogos\logo-words-only.png" className="App-logo-words" alt="logo-words" />
+    <div className="App" id = "home-section">
+      <header className="App-header" ref={headerRef} style={{height: "300px"}}>
+        <div className="inner-header">
+          <div className="logo-side" ref={logoContainer}>
+            <img src="/tdllogos/TDL-logo-color-cropped.png" className="App-logo" alt="logo" />
+            <img src="/tdllogos/logo-words-only.png" className="App-logo-words" alt="logo-words" />
           </div>
-          <div class="nav-items">
-            <a class="a" href="#home-section">Home</a>
-            <a class="a" href="#about-section">About Us</a>
-            <a class="a" href="#services-section">Services</a>
-            <a class="a" href="#project-section">Projects</a>
-            <a class="contact" href="#contact-section"><span class="contact-help"></span>Contact Us</a>
+          <div className="nav-items">
+            <a className="a" href="#home-section">Home</a>
+            <a className="a" href="#about-section">About Us</a>
+            <a className="a" href="#services-section">Services</a>
+            <a className="a" href="#project-section">Projects</a>
+            <a className="contact" href="#contact-section"><span className="contact-help"></span>Contact Us</a>
           </div>
         </div>
       </header>
-      <body class = "body">
-        <div class = "space"></div>
-        <div class = "inner-body">
-          <div class="img-phrase-pair">
-            <div class ="left-phrase">
+      <body className = "body">
+        <div className = "space"></div>
+        <div className = "inner-body">
+          <div className="img-phrase-pair">
+            <div className ="left-phrase">
               <h1>We Are A Family</h1>
               <h3>A company that builds from the heart</h3>
             </div>
             <img className="img" alt="show-the-back" src="photos/the-back.png"></img>
-            <div class= "bottom-text-clear">
-              <p class = "para">When our founder, Dan Lokeni, tragically lost his son, Ta'imua'iao Dayne Lokeni (TDL), he envisioned a 
+            <div className= "bottom-text-clear">
+              <p className= "para">When our founder, Dan Lokeni, tragically lost his son, Ta'imua'iao Dayne Lokeni (TDL), he envisioned a 
                 company that could uplift his community and honor his son's memory. At TDL, you're a part of something bigger, where 
                 every project serves a greater purpose. </p>
-              <p class = "para">We operate like an employee-owned non-profit, distributing earnings equitably and 
+              <p className= "para">We operate like an employee-owned non-profit, distributing earnings equitably and 
                 supporting philanthropic projects to fight poverty in Samoa. This motivates our team to work smarter and faster, 
                 benefiting everyone. Unlike other steel erectors, we're driven to maximize efficiency.</p>
             </div>
           </div>
-          <div class="img-phrase-pair" id="about-section">
-            <h1 class="center-phrase">About Us</h1>
-            <img class="img" alt="say-hi" src="photos/the-gang.png"></img>
-            <div class="mid-text-clear">
+          <div className="img-phrase-pair" id="about-section">
+            <h1 className="center-phrase">About Us</h1>
+            <img className="img" alt="the-gang" src="/photos/the-gang-001.png"></img>
+            <div className="mid-text-clear">
               <p>TDL is a trusted steel erection company in Utah with over 15 years of experience in the structural 
                 steel trade. We specialize in commercial, industrial, and public works projects, handling everything from steel joists 
                 and girders to beams and cross-sections for buildings of all sizes. 
@@ -78,49 +78,48 @@ function App() {
                 or repairing existing ones, our skilled team brings the expertise and dedication to ensure your project's success.</p>
             </div>
           </div>
-          <div class="img-phrase-pair" id="services-section">
-            <h1 class="left-phrase">Our Provided Services</h1>
-            <img class="img" alt="services" src="photos/far-stare.png"></img>
-            <ul class="right-panel-text">
+          <div className="img-phrase-pair" id="services-section">
+            <h1 className="left-phrase">Our Provided Services</h1>
+            <img className="img" alt="services" src="photos/far-stare.png"></img>
+            <ul className="right-panel-text">
               <li>Commercial</li>
               <li>Industrial</li>
               <li>Public Works</li>
               <li>Buildings Of All Sizes</li>              
             </ul>
           </div>
-          <div class="img-phrase-pair" id="project-section">
-            <h1 class="corner-phrase">Our Latest Project</h1>
-            <img class="img" alt="say-hi" src="photos/project-photos/library-full.jpg"></img>
-            <div class="right-panel-text">
+          <div className="img-phrase-pair" id="project-section">
+            <h1 className="corner-phrase">Our Latest Project</h1>
+            <img className="img" alt="say-hi" src="photos/project-photos/library-full.jpg"></img>
+            <div className="right-panel-text">
               <h2>The Spanish Fork Library Hall</h2>
               <h3>Completed April 29, 2023</h3>
             </div>
           </div>
-          <div class="img-phrase-pair" id="contact-section">
-            <div class = "right-phrase">
-              <h2 class="h1-contact">Contact Us</h2>
-              <div class ="footer-para">
+          <div className="img-phrase-pair" id="contact-section">
+            <div className = "right-phrase">
+              <h2 className="h1-contact">Contact Us</h2>
+              <div className ="footer-para">
                 <h4>Located in: West Jordan, UT 84081, USA</h4>
                 <h4>Call us: (801) 259-6255</h4>
-                <h4>Email us: tdl23group@gmail.com</h4>
+                <h4><a href="#contact-sectionw" id="openPopup">Email us here</a></h4>
               </div>
             </div>
-            <img class="img" alt="say-hi" src="photos/look-up-short.png"></img>
+            <img className="img" alt="say-hi" src="photos/look-up-short.png"></img>
           </div>
-          <div class = "end-space"></div>
         </div>
       </body>
       <footer className="App-footer">
-        <p class="copyright">© David Wells 2024. All Rights Reserved. **Disclaimer: This is a site proposal, not an official site of TDL.**</p>
-        <div class="media-links">
-          <a class ="icon" href = "https://www.facebook.com/TDLGROUP/">
-            <img class="icon" alt="facebook" src="tdllogos/facebook.jpg"></img>
+        <p className="copyright">© David Wells 2024. All Rights Reserved. **Disclaimer: This is a site proposal, not an official site of TDL.**</p>
+        <div className="media-links">
+          <a className ="icon" href = "https://www.facebook.com/TDLGROUP/">
+            <img className="icon" alt="facebook" src="/tdllogos/facebook.jpg"></img>
           </a>
-          <a class = "icon" href = "https://www.facebook.com/TDLGROUP/videos/982365362379517/">
-            <img class="icon" alt="youtube" src="tdllogos/youtube.jpg"></img>
+          <a className = "icon" href = "https://www.facebook.com/TDLGROUP/videos/982365362379517/">
+            <img className="icon" alt="youtube" type="image/jpg" src="/tdllogos/youtube.jpg"></img>
           </a>
-          <a class = "icon" href = "https://www.linkedin.com/company/tdl-group-llc/">
-            <img class="icon" alt="linkedin" src="tdllogos/linkedin.jpg"></img>
+          <a className = "icon" href = "https://www.linkedin.com/company/tdl-group-llc/">
+            <img className="icon" alt="linkedin" src="/tdllogos/linkedin.jpg"></img>
           </a>
         </div>
       </footer>
