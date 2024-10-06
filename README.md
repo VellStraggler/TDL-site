633fd8badb4817b323be032608640812ca64036c
VellStraggler <david@incwells.com>
2024-10-05 21:55:39 -0600

    server-reader

--- README.md
+++ README.md
@@ -9,19 +9,16 @@ Time alotted to project as of 9/27: 8 hours
 - working social media icon links
 - connected github repository
 - connect header links to each respective section
+- Set section headers to roll out when reached
 
 ## features to add:
+- add services section
 - multiple latest projects added (found on Linkedin)
-- Set section headers to roll out when reached
 - going online through AWS
 - Functional Contact section
 - "Vision" scrolling: the images scroll at a different pace then the rest of the site
 - section text appears and disappears based on which image you are seeing at the time
 - Footer section expands at the bottom into the "Contact Us" section
-- Add disclaimer saying this site is a proposal / mockup
-- Add personal copyright claiming ownership over design: © David Wells 2024. All Rights Reserved.
-- Add what TDL stands for in the "About Us" section
-- Use this font family for "About Us": 
 - Add small caption to each photo to credit where it came from
 - Add an email form that does not directly send the email
 
--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       if (window.scrollY < 0) {
         window.scrollTo(0,0);
       }
-
       if (window.scrollY > 50) {
         if (headerRef.current) {
           headerRef.current.style.height = "130px";
@@ -19,7 +18,7 @@ function App() {
         }
       } else {
         if (headerRef.current) {
-          headerRef.current.style.height = "300px";
+          headerRef.current.style.height = "250px";
           logoContainer.current.classList.remove('shrunk');
         }
       }
@@ -42,53 +41,71 @@ function App() {
           <div class="nav-items">
             <a class="a" href="#home-section">Home</a>
             <a class="a" href="#about-section">About Us</a>
+            <a class="a" href="#services-section">Services</a>
             <a class="a" href="#project-section">Projects</a>
             <a class="contact" href="#contact-section"><span class="contact-help"></span>Contact Us</a>
           </div>
         </div>
       </header>
       <body class = "body">
+        <div class = "space"></div>
         <div class = "inner-body">
-          <div class = "space"></div>
           <div class="img-phrase-pair">
-            <div class ="phrase">
+            <div class ="left-phrase">
               <h1>We Are A Family</h1>
-              <h2>A company that builds from the heart</h2>
+              <h3>A company that builds from the heart</h3>
             </div>
             <img className="img" alt="show-the-back" src="photos/the-back.png"></img>
-            <p class = "para">When our founder, Dan Lokeni, tragically lost his son, Ta'imua'iao Dayne Lokeni (TDL), he envisioned a 
-              company that could uplift his community and honor his son's memory. At TDL, you're a part of something bigger, where 
-              every project serves a greater purpose. </p>
-            <p class = "para">We operate like an employee-owned non-profit, distributing earnings equitably and 
-              supporting philanthropic projects to fight poverty in Samoa. This motivates our team to work smarter and faster, 
-              benefiting everyone. Unlike other steel erectors, we're driven to maximize efficiency.</p>
+            <div class= "bottom-text-clear">
+              <p class = "para">When our founder, Dan Lokeni, tragically lost his son, Ta'imua'iao Dayne Lokeni (TDL), he envisioned a 
+                company that could uplift his community and honor his son's memory. At TDL, you're a part of something bigger, where 
+                every project serves a greater purpose. </p>
+              <p class = "para">We operate like an employee-owned non-profit, distributing earnings equitably and 
+                supporting philanthropic projects to fight poverty in Samoa. This motivates our team to work smarter and faster, 
+                benefiting everyone. Unlike other steel erectors, we're driven to maximize efficiency.</p>
+            </div>
           </div>
           <div class="img-phrase-pair" id="about-section">
-            <h1 class="phrase">About Us</h1>
+            <h1 class="center-phrase">About Us</h1>
             <img class="img" alt="say-hi" src="photos/the-gang.png"></img>
-            <p class ="para">TDL is a trusted steel erection company in Utah with over 15 years of experience in the structural 
-              steel trade. We specialize in commercial, industrial, and public works projects, handling everything from steel joists 
-              and girders to beams and cross-sections for buildings of all sizes. 
-            </p>
-            <p class = "para">We are committed to delivering high-quality, safe, 
-              and efficient work that meets the expectations of our general contractors. Whether constructing new steel structures 
-              or repairing existing ones, our skilled team brings the expertise and dedication to ensure your project's success.</p>
+            <div class="mid-text-clear">
+              <p>TDL is a trusted steel erection company in Utah with over 15 years of experience in the structural 
+                steel trade. We specialize in commercial, industrial, and public works projects, handling everything from steel joists 
+                and girders to beams and cross-sections for buildings of all sizes. 
+              </p>
+              <p>We are committed to delivering high-quality, safe, 
+                and efficient work that meets the expectations of our general contractors. Whether constructing new steel structures 
+                or repairing existing ones, our skilled team brings the expertise and dedication to ensure your project's success.</p>
+            </div>
+          </div>
+          <div class="img-phrase-pair" id="services-section">
+            <h1 class="left-phrase">Our Provided Services</h1>
+            <img class="img" alt="services" src="photos/far-stare.png"></img>
+            <ul class="right-panel-text">
+              <li>Commercial</li>
+              <li>Industrial</li>
+              <li>Public Works</li>
+              <li>Buildings Of All Sizes</li>              
+            </ul>
           </div>
           <div class="img-phrase-pair" id="project-section">
-            <h1 class="phrase">Our Latest Projects</h1>
+            <h1 class="corner-phrase">Our Latest Project</h1>
             <img class="img" alt="say-hi" src="photos/project-photos/library-full.jpg"></img>
-            <p class = "para"></p>
+            <div class="right-panel-text">
+              <h2>The Spanish Fork Library Hall</h2>
+              <h3>Completed April 29, 2023</h3>
+            </div>
           </div>
           <div class="img-phrase-pair" id="contact-section">
-            <div class = "phrase">
-              <h1>Contact Us</h1>
+            <div class = "right-phrase">
+              <h2 class="h1-contact">Contact Us</h2>
               <div class ="footer-para">
-                <h3>Located in: West Jordan, UT 84081, USA</h3>
-                <h3>Call us: (801) 259-6255</h3>
-                <h3>Email us: tdl23group@gmail.com</h3>
+                <h4>Located in: West Jordan, UT 84081, USA</h4>
+                <h4>Call us: (801) 259-6255</h4>
+                <h4>Email us: tdl23group@gmail.com</h4>
               </div>
             </div>
-            <img class="img" alt="say-hi" src="photos/look-up.png"></img>
+            <img class="img" alt="say-hi" src="photos/look-up-short.png"></img>
           </div>
           <div class = "end-space"></div>
         </div>
